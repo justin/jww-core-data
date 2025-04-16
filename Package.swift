b9f95d0c3602e06d2cdddd3241e9435c1262021d
Justin Williams <justinw@me.com>
2025-04-16 09:52:31 -0600

    Update minimum platforms to iOS 15 / macOS 12

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "JWW Core Data",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11),
-        .tvOS(.v14),
-        .watchOS(.v7)
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8)
     ],
     products: [
         .library(name: "JWW Core Data", targets: ["JWWCoreData"])
